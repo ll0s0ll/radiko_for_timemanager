312033ec537219142e64e5746985f039760c3fba
ShunITO <shunito.s110@gmail.com>
2018-03-21 20:50:17 +0900

    一部不具合修正。README修正。

--- radiko_for_timemanager.py
+++ radiko_for_timemanager.py
@@ -237,7 +237,11 @@ if __name__ == '__main__':
 
         while not is_force_termination:
             
+<<<<<<< HEAD
             if execute(cmd) != 0:
+=======
+            if execute(cmd) == 1:
+>>>>>>> sandbox
                 sys.exit(1)
 
             if is_force_termination:
